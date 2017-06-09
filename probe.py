72a43bd944dd9ac55d0d966048944c2795c266b7
JuanKman94 <juan.carlos.menonita@gmail.com>
2017-06-09 09:33:17 -0500

    Test for multithreaded probing. A thread for listening and another for broadcasting

--- probe.py
+++ probe.py
@@ -1,34 +1,81 @@
 #!/usr/bin/env python3
 
-import automata
+import time
 import socket
+import automata
+import threading
 import netifaces
 
 PORT = 3000
+MAX_LEN = 256
 
-ifaces = automata.get_ifaces()
+def listen_handler(sock, addr):
+    print('Listening on {0}:{1}...'.format(addr[0], addr[1]))
+    sock.bind( addr )
+    try:
+        while True:
+            conn = sock.recvfrom(MAX_LEN)
 
-for key in ifaces:
-    iface = ifaces[key]
-    addr = (iface['broadcast'], PORT)
+            msg = '''Data from:
+      * {ip}
+      * {s}'''.format(ip = conn[1][0],
+                s = str(conn[0], encoding='utf-8')
+                )
+
+            print(msg)
+    except:
+        sock.close()
+
+    return 0
+
+def broadcast_handler(sock, addr):
     data = "pinging {iface} ({ip}:{port})\r\n".format(
                 iface = key,
                 ip = addr[0],
                 port = addr[1]
             ).encode()
 
-    print("Sending data:\n  * To {0}".format(addr[0]))
-    print('  * Message "{0}"'.format(str(data, encoding='utf-8').rstrip()))
+    print('Broadcasting to {0}:{1}...'.format(addr[0], addr[1]))
+    try:
+        while True:
+            sock.sendto(data, addr)
+            time.sleep(1)
+    except:
+        sock.close()
 
-    s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    return 0
 
-    s.sendto(data, addr)
-    res = s.recvfrom(256)
+ifaces = automata.get_ifaces()
+k = 'eth0'
 
-    print('Received data:\n  * From {0}'.format(res[1][0]))
-    print('  * Message: "{0}"'.format(str(res[0], encoding='utf-8').rstrip()))
+for key in ifaces:
+    k = key
 
-    s.close()
+iface = ifaces[k]
+addr = (iface['broadcast'], PORT)
+my_addr = (iface['addr'], PORT)
+
+### Broadcaster
+broadcaster = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+t_broadcast = threading.Thread(
+        target = broadcast_handler,
+        args = (broadcaster, addr)
+)
+t_broadcast.start()
+
+### Listener
+listener = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+
+t_listen = threading.Thread(
+        target = listen_handler,
+        args = (listener, my_addr)
+)
+t_listen.start()
+
+
+t_broadcast.join()
+t_listen.join()
 
 print("\n\nexiting...")
