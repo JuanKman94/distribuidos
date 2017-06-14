c4ce1cca9fc27c60732e5c2c1df951ed3bbceba7
JuanKman94 <juan.carlos.menonita@gmail.com>
2017-06-13 23:21:06 -0500

    Simplify automata methods. Work on UDP and handle exceptions by thread

--- automata.py
+++ automata.py
@@ -7,139 +7,55 @@ import psutil
 import netaddr
 import platform
 import netifaces
-import threading
+import threading as ting
 import socket as sock
 
+PORT = 3000
+MAX_CON = 5
+MAX_MSG_LEN = 512
 MAX_FAILED_ATTEMPTS = 10
 
 class automata(sock.socket):
-    def __init__(self, address = '', port = 9999, max_conn = 5):
+    def __init__(self, address = '0.0.0.0', port = PORT, max_conn = MAX_CON):
         self.address = address
         self.port = port
-        self.max_conn = 5
-        self.role = 'server' # [ server | master ]
-        self.master = None # ip address for current master
-        self.ifaces = None
+        self.max_conn = max_conn
+        self.pals = dict()
 
         try:
             # AF_INET for IPv4
             # SOCK_STREAM for TCP, SOCK_DGRAM for UDP
-            super().__init__( sock.AF_INET, sock.SOCK_STREAM )
+            super().__init__( sock.AF_INET, sock.SOCK_DGRAM )
         except sock.error as ex:
             print('Error creating socket. {0}'.format(ex))
 
-    def beacon(self):
+    def beacon(self, addr):
         '''Send beacon to entire network to look for other automatas'''
-        ifaces = get_ifaces()
-        self.master = None
-
-        for iface in ifaces:
-            cidr = netaddr.IPNetwork(ifaces[iface]['addr'], ifaces[iface]['netmask'])
-
-            if self.master: break
-
-            hosts = [ ip for ip in cidr.iter_hosts() ]
-
-            for ip in hosts:
-                print('connecting {ip}... '.format(ip = ip), end='')
-                try:
-                    self.connect( (str(ip), self.port) )
-                    print('successful')
-                    self.master = str(ip)
-                    break
-                except ConnectionRefusedError:
-                    print('refused')
-                except OSError as ex:
-                    print('no route {0}'.format(ex))
-                else:
-                    print('error')
-
-        return self.master
+        print('broadcasting CPU data on port {p}'.format(p = addr[1]))
+        try:
+            while True:
+                data = json.dumps(get_cpu_data()).encode()
+                self.sendto(data, addr)
+                time.sleep(3)
+        except Exception as ex:
+            print('error [{t}]!'.format(t = ting.current_thread().name), ex)
 
     def up(self):
         try:
-            self.bind( (self.address, self.port) )
-            self.listen(self.max_conn)
-            print('Socket binded on {0}:{1}'.format(self.address, self.port))
+            print('listening on port {p}'.format(p = self.port))
+            self.bind( ('0.0.0.0', self.port) )
 
             while True:
-                (conn, addr) = self.accept() # blocking call
-                print('Connected with {0}'.format(addr[0]))
-                threading.Thread( target = self.handle_conn, args = (conn, addr)).start()
+                conn = self.recvfrom(MAX_MSG_LEN) # blocking call
+                ip = conn[1][0]
+                try:
+                    data = json.loads(str(conn[0], encoding='utf-8'))
+                except Exception as ex:
+                    data = { "error": str(ex) }
+
+                self.pals[ip] = data
         except Exception as ex:
-            print('Closing server socket({0}). Sleeping 10 seconds...'.format(ex))
-            self.close()
-            time.sleep(10)
-            return 1
-
-    def handle_conn(self, conn, addr):
-        try:
-            while True:
-                data = str(conn.recv(1024), encoding='utf-8')
-                while data:
-                    print(data)
-                    conn.sendall('OK\r\n'.encode())
-                    data = str(conn.recv(1024), encoding='utf-8')
-        except:
-            if conn: conn.close()
-
-        print('Closing connection with {0}'.format(addr[0]))
-
-    def ping(self):
-        '''Send info to master'''
-        count, failed_attempts = 0, 0
-        failure, no_master_found = False, False
-
-        while True:
-            try:
-                message = str( get_cpu_data() )
-                self.sendall(message.encode())
-            except Exception as ex:
-                failure = True
-
-            if failure:
-                failed_attempts += 1
-            else:
-                reply = str(self.recv(1024), encoding = 'utf-8')
-                print(count, '. ', reply.rstrip() )
-                count += 1
-                time.sleep(1)
-
-            if failed_attempts >= MAX_FAILED_ATTEMPTS:
-                no_master_found = True
-                break
-
-        return None
-
-    def is_master(self):
-        return self.role == 'master'
-
-    def become_master(self):
-        self.role = 'master'
-        self.address = self.get_ipv4()
-
-        if not self.address:
-            print('Error getting IPv4 address.')
-            sys.exit(1)
-
-        try:
-            self.up()
-        except:
-            return -1
-
-        return 1
-
-    def get_ipv4(self):
-        ifaces = get_ifaces()
-        for iface in ifaces:
-            cidr = netaddr.IPNetwork(ifaces[iface]['addr'], ifaces[iface]['netmask'])
-            try:
-                cidr.ipv4() # is it a IPv4 network?
-                return ifaces[iface]['addr']
-            except netaddr.AddrConversionError:
-                return None
-
-        return None
+            print('error [{t}]!'.format(t = ting.current_thread().name), ex)
 
 def get_cpu_data():
     mem, arch = psutil.virtual_memory()[0], platform.machine()
@@ -150,11 +66,11 @@ def get_cpu_data():
     mem /= 1024
 
     return {
-            #'hdd': '32GB',
-            'mem': str(mem) + 'GB',
-            #'arch': arch,
-            'os': os,
-            'proc': proc
+        #'hdd': '32GB',
+        'mem': str(mem) + 'GB',
+        #'arch': arch,
+        'os': os,
+        'proc': proc
     }
 
 def get_ifaces():
